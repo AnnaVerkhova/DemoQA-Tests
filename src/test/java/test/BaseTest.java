f2d6144f5e07f36e0c3178e040224ecda7f6d80a
Anna Verkhova <werxowaanna1991@gmail.com>
2026-03-24 20:31:28 +0300

    remove old BaseTest

--- src/test/java/test/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class BaseTest {
-    protected static TestConfig config =
-            ConfigFactory.create(TestConfig.class);
-}
